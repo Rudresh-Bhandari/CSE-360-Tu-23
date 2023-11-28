483448294b64a86507d81349918c1c47ccd453db
Rudresh Bhandari <rudreshbhandari382@gmail.com>
2023-11-28 11:17:33 -0700

    junit test added for multifactor auth

--- src/application/maintest.java
+++ src/application/maintest.java
@@ -7,8 +7,24 @@ import org.junit.jupiter.api.Test;
 class maintest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	// according to my logic and set up , the length of the code should always be 4 
+	void codeLength() {
+		Main main = new Main();
+		String mfaCode = main.generateCode();
+		assertEquals(4,mfaCode.length(),"The code length should be 4");
 	}
-	
+	@Test
+	void checkIfUniqe() {
+		Main main = new Main();
+		String first=main.generateCode();
+		String second = main.generateCode();
+		assertNotEquals(first,second,"The two codes should not be identical");
+	}
+	@Test 
+	void codenumeric() {
+		Main main = new Main();
+		String code = main.generateCode();
+		assertTrue(code.matches("\\d{4}"),"Code should be numeric");
+	}
+
 }
